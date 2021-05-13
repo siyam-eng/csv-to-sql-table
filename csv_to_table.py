cdae5a94dbbbd0e32cb47e1f9fef7f47073303bc
Siyam Ahmed <siyamraisa@gmail.com>
2021-05-14 00:54:51 +0600

    make the csv_to_table file work.

--- csv_to_table.py
+++ csv_to_table.py
@@ -1,22 +1,47 @@
 """Read assets from given csv file, validate the data and add them to a SQL Table"""
 
+import csv
 import random
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import insert
+from credentials import username, password
+from datetime import datetime
+from models import Asset
+
+
+username = username
+password = password
+host = "localhost:3306"
+dbname = "csvtotable"
+
+engine = create_engine(
+    f"mysql+pymysql://{username}:{password}@{host}/{dbname}?charset=utf8mb4"
+)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+my_sess = Session()
+
 
 CATEGORIES = {
     1: {"name": "Television", "shortname": "TE"},
     2: {"name": "Switch", "shortname": "SW"},
     3: {"name": "BOX", "shortname": "BO"},
     4: {"name": "LAPTOP", "shortname": "LA"},
-
+    5: {"name": "LAMP", "shortname": "LM"},
+    6: {"name": "WASHING MACHINE", "shortname": "WA"},
+    7: {"name": "WATCH", "shortname": "WT"},
 }
 
 
+
 # Asset id starts with category short name and has 8 random digits afterwards
-def assetid_is_valid(category_shortname, asset_id):
-    if len(asset_id) == 10:
-        if asset_id.lower().startswith(category_shortname.lower()):
-            if asset_id[2:].isdigit():
-                return True
+def assetid_is_valid(assetid, category_shortname):
+    if len(assetid) == 10:
+        if assetid.lower().startswith(category_shortname.lower()):
+            if assetid[2:].isdigit():
+                return "YES"
             return (
                 "Failed: Asset id does not contain 8 digits after category short name"
             )
@@ -24,35 +49,131 @@ def assetid_is_valid(category_shortname, asset_id):
     return "Failed: Asset id does not have 10 characters"
 
 
-# TODO check if asset id is unique
-def assetid_is_unique(assetid, db_assetids):
-    return True
-    pass
+# check if asset id is unique
+def assetid_is_unique(assetid, db_session, Asset):
+    asset = db_session.query(Asset).filter_by(assetId=assetid).first()
+    if not asset:
+        return "YES"
+    return "Failed: Given assetId already exists"
 
 
 # generate new asset id with the given category short name
-def generate_assetid(category_shortname, db_assetids):
-    while True:
-        digits = "".join([str(random.randint(0, 9)) for i in range(8)])
-        assetid = category_shortname.upper() + digits
+def generate_assetid(category_shortname, db_session):
+    if category_shortname:
+        while True:
+            digits = "".join([str(random.randint(0, 9)) for i in range(8)])
+            assetid = category_shortname.upper() + digits
 
-        if assetid_is_unique(assetid, db_assetids):
-            return assetid
+            if assetid_is_unique(assetid, db_session):
+                return assetid
 
 
 # if neither asset id nor category is provided flag error
 def assetid_and_category_exists(assetid, category):
     # both are present
     if bool(assetid and category):
-        return True
+        return "YES"
     # if asset id is provided and category is not provided flag error
     elif assetid:
-        return 'Failed: Asset id is provided but category is not provided'
+        return "Failed: Asset id is provided but category is not provided"
     # both are absent
-    return 'Failed: Asset id and category is not provided'
+    return "Failed: Asset id and category is not provided"
 
 
 
-# print(assetid_is_valid("CU", generate_assetid("cu", set())))
+def insert_asset(session, Model, **kwargs):
+    # creating a movie object -> row in the table
+    new_asset = Model(**kwargs)
+    # adding the row
+    session.add(new_asset)
+    # commiting the row
+    session.commit()
 
-print(assetid_and_category_exists('TV567898', ''))
+    return new_asset
+
+
+# Open the file to work with
+with open("asset.csv", "r") as f:
+    csv_reader = csv.DictReader(f)
+
+    with open('error.csv', 'w') as f:
+        field_names = ['Name','Asset ID','Description','Location','Tags','Status','Category','Metadata','Field1','Field2','Field3','Comments ']
+        csv_writer = csv.DictWriter(f, field_names)
+        csv_writer.writeheader()
+
+        for row in csv_reader:
+
+            # Retrieve the data
+            assetId = row["Asset ID"]  # not nullable
+            assetName = row["Name"]   # not nullable
+            assetDescription = row["Description"]
+            assetLocation = row['Location']
+            assetTags = row["Tags"]
+            assetStatus = row["Status"]  # not nullable
+            assetCategory_id = row["Category"]  # not nullable
+            assetMetadata = row["Metadata"]
+            userField1 = row["Field1"]
+            userField2 = row["Field2"]
+            userField3 = row["Field3"]
+
+            # TODO flag error if not nullable fields are null
+            if assetId and assetName and assetStatus and assetCategory_id:
+
+                # check if categoryId is in the given categories
+                if assetCategory_id and assetCategory_id.isdigit() and int(assetCategory_id) in CATEGORIES:
+                    category_shortname = CATEGORIES[int(assetCategory_id)]["shortname"]
+                
+                    # generate an asset id if it does not exists
+                    assetId = generate_assetid(category_shortname, my_sess) if not assetId else assetId
+
+                    # check existence
+                    if (existence := assetid_and_category_exists(assetId, assetCategory_id)) == "YES":
+                            # check the validity of the assetId
+                            if (validity := assetid_is_valid(assetId, category_shortname)) == "YES":
+                                # check the uniqueness of assetId
+                                if (uniqueness := assetid_is_unique(assetId, my_sess, Asset)) == "YES":
+                                    # add the asset to database
+                                    insert_asset(
+                                        my_sess,
+                                        Asset,
+                                        assetId=assetId,
+                                        assetName=assetName,
+                                        assetDescription=assetDescription,
+                                        assetLocation=assetLocation,
+                                        assetTags=assetTags,
+                                        assetStatus=assetStatus,
+                                        assetMetadata=assetMetadata,
+                                        assetCategory_id=assetCategory_id,
+                                        _created=datetime.now(),
+                                        _updated=datetime.now(),
+                                        userField1=userField1,
+                                        userField2=userField2,
+                                        userField3=userField3,
+                                    )
+                                else:
+                                    row['Comments '] = uniqueness
+                                    csv_writer.writerow(row)
+                            else:
+                                row['Comments '] = validity
+                                csv_writer.writerow(row)
+                    else:
+                        row['Comments '] = existence
+                        csv_writer.writerow(row)
+
+                else:
+                    row['Comments '] = 'Failed: Category Id not in category mapping'
+                    csv_writer.writerow(row)
+
+            # Error messages if one of the required fields are not available
+            elif not assetId:
+                row['Comments '] = 'Failed: The Field "assetId" is required'
+                csv_writer.writerow(row)
+            elif not assetName:
+                row['Comments '] = 'Failed: The Field "assetName" is required'
+                csv_writer.writerow(row)
+            elif not assetStatus:
+                row['Comments '] = 'Failed: The Field "assetStatus" is required'
+                csv_writer.writerow(row)
+            elif not assetCategory_id:
+                row['Comments '] = 'Failed: The Field "assetCategory_id" is required'
+                csv_writer.writerow(row)
--- models.py
+++ models.py
@@ -1,17 +1,200 @@
-from sqlalchemy import create_engine, Column, String, Integer, Table
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (
+    create_engine,
+    Column,
+    String,
+    Integer,
+    Text,
+    ForeignKey,
+    DateTime,
+    Enum,
+)
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from credentials import username, password
-
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 username = username
 password = password
-host = 'localhost:3306'
-dbname = 'world'
+host = "localhost:3306"
+dbname = "csvtotable"
 
 
 # MySQL engine
-engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{dbname}?charset=utf8mb4")
+engine = create_engine(
+    f"mysql+pymysql://{username}:{password}@{host}/{dbname}?charset=utf8mb4"
+)
+Base = declarative_base()
 Session = sessionmaker(bind=engine)
 my_sess = Session()
-Base = declarative_base()
+
+
+class Asset(Base):
+    __tablename__ = "asset"
+
+    id = Column(Integer, primary_key=True)
+    assetId = Column(String(10), nullable=False, unique=True)
+    assetName = Column(String(255), nullable=False)
+    assetDescription = Column(Text)
+    assetLocation = Column(Text)
+    assetTags = Column(Text)
+    assetStatus = Column(Enum("ACTIVE", "INACTIVE"), nullable=False)
+    _created = Column(DateTime, nullable=False)
+    _updated = Column(DateTime, nullable=False)
+    assetCategory_id = Column(
+        Integer, ForeignKey("assetCategories.id"), nullable=False, index=True
+    )
+    assetCategory = relationship(
+        "AssetCategory",
+        primaryjoin="Asset.assetCategory_id == AssetCategory.id",
+        backref="assets",
+    )
+    assetMetadata = Column(Text)
+    _etag = Column(String(50))
+    userField1 = Column(String(125))
+    userField2 = Column(String(125))
+    userField3 = Column(String(125))
+    userField4 = Column(String(125))
+    userField5 = Column(String(125))
+
+
+class AssetCategory(Base):
+    __tablename__ = "assetCategories"
+
+    id = Column(Integer, primary_key=True)
+    assetCategoryName = Column(String(255), nullable=False, unique=True)
+    assetCategoryLogo = Column(Text, nullable=False)
+    editable = Column(Integer, nullable=False, default=1)
+    _created = Column(DateTime, nullable=False)
+    _updated = Column(DateTime, nullable=False)
+    _etag = Column(String(50))
+    shortName = Column(String(2), unique=True, nullable=False)
+
+
+# Create the table(s)
+# Base.metadata.create_all(engine)
+
+
+# Take the column values and create a new object (row in the table)
+def insert_object(session, Model, **kwargs):
+    # creating a movie object -> row in the table
+    new_obj = Model(**kwargs)
+    # adding the row
+    session.add(new_obj)
+    # commiting the row
+    session.commit()
+
+    return new_obj
+
+
+# asset = my_sess.query(Asset).filter_by(assetId='LA12345678').first()
+def insert_categories():
+    insert_object(
+        
+        my_sess,
+        AssetCategory,
+        assetCategoryName="TELEVISION",
+        shortName="TE",
+        assetCategoryLogo="Logo",
+        editable=1,
+        _created=datetime.now(),
+        _updated=datetime.now(),
+        _etag="Etag for switch",
+    )
+    insert_object(
+        my_sess,
+        AssetCategory,
+        assetCategoryName="SWITCH",
+        shortName="SW",
+        assetCategoryLogo="Logo",
+        editable=1,
+        _created=datetime.now(),
+        _updated=datetime.now(),
+        _etag="Etag for switch",
+    )
+
+    insert_object(
+        my_sess,
+        AssetCategory,
+        assetCategoryName="BOX",
+        shortName="BO",
+        assetCategoryLogo="Logo",
+        editable=1,
+        _created=datetime.now(),
+        _updated=datetime.now(),
+        _etag="Etag for switch",
+    )
+    insert_object(
+        my_sess,
+        AssetCategory,
+        assetCategoryName="LAPTOP",
+        shortName="LA",
+        assetCategoryLogo="Logo",
+        editable=1,
+        _created=datetime.now(),
+        _updated=datetime.now(),
+        _etag="Etag for switch",
+    )
+
+    insert_object(
+        my_sess,
+        AssetCategory,
+        assetCategoryName="Lamp",
+        shortName="LM",
+        assetCategoryLogo="Logo",
+        editable=1,
+        _created=datetime.now(),
+        _updated=datetime.now(),
+        _etag="Etag for switch",
+    )
+
+    insert_object(
+        my_sess,
+        AssetCategory,
+        assetCategoryName="WASHING MACHINE",
+        shortName="WA",
+        assetCategoryLogo="Logo",
+        editable=1,
+        _created=datetime.now(),
+        _updated=datetime.now(),
+        _etag="Etag for switch",
+    )
+
+    insert_object(
+        my_sess,
+        AssetCategory,
+        assetCategoryName="WATCH",
+        shortName="WT",
+        assetCategoryLogo="Logo",
+        editable=1,
+        _created=datetime.now(),
+        _updated=datetime.now(),
+        _etag="Etag for switch",
+    )
+
+
+
+
+
+# # Name,Asset ID,Description,Location,Tags,Status,Category,Metadata,Field1,Field2,Field3,Comments
+# insert_object(
+#     my_sess,
+#     Asset,
+#     assetId="SW12345677",
+#     assetName="Switch 2",
+#     assetDescription="This is a switch",
+#     assetLocation="UK",
+#     assetTags='switch',
+#     assetStatus="ACTIVE",
+#     assetMetadata="DOP:17/12/2015",
+#     assetCategory_id=2,
+#     _created=datetime.now(),
+#     _updated=datetime.now(),
+#     _etag='laptop e tag',
+#     userField1='',
+#     userField2='',
+#     userField3='',
+#     userField4='',
+
+# )
+
